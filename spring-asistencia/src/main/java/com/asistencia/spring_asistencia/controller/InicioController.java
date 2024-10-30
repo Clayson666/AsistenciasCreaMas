78d2d3223effb103350d2d7dfb39151565022855
Clayson666 <emanuel.cabanillas4b@gmail.com>
2024-10-30 18:39:13 -0500

    Agregando vista de edicion

--- spring-asistencia/src/main/java/com/asistencia/spring_asistencia/controller/InicioController.java
+++ spring-asistencia/src/main/java/com/asistencia/spring_asistencia/controller/InicioController.java
@@ -29,14 +29,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author eulal
@@ -118,6 +111,17 @@ public class InicioController {
 
         return "redirect:/creandos?idLugar=" + idLugar;
 
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEdicion(@PathVariable("id") Integer id, Model model){
+        Optional<Asistencia> asistencia = asistenciaService.get(id);
+        if(asistencia.isPresent()){
+            model.addAttribute("asistencia",asistencia.get());
+            return "usuario/editarCreandos";
+        }else{
+            return "redirect:/error";
+        }
     }
 
     @GetMapping("/verAsistenciasCreandos")
